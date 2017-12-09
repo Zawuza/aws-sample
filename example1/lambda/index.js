f7e30fb4e7ae0b7eac56428a0497e80ad1fa022a
ZAwuza <andreizawuza@gmail.com>
2017-12-09 15:57:16 +0100

    Add deployment zip + modify index.js

--- example1/lambda/index.js
+++ example1/lambda/index.js
@@ -1,10 +1,18 @@
-var exec = require('child_process').exec;
-exports.handler = function(event, context) {
-  child = exec("./hello", function(error) {
-    // Resolve with result of process
-    context.done(error, 'Process complete!');
-  });
-  // Log process stdout and stderr
-  child.stdout.on('data', console.log);
-  child.stderr.on('data', console.error);
+'use strict';
+
+const exec = require('child_process').exec;
+
+exports.handler = (event, context, callback) => {
+    event.cmd = "./hello";
+    if (!event.cmd) {
+        return callback('Please specify a command to run as event.cmd');
+    }
+    const child = exec(event.cmd, (error) => {
+        // Resolve with result of process
+        callback(error, 'Process complete!');
+    });
+
+    // Log process stdout and stderr
+    child.stdout.on('data', console.log);
+    child.stderr.on('data', console.error);
 };
